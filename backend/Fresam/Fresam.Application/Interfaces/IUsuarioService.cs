8d9e0b17eafbc634baceb67d570348450304527b
Victor Alfonso Flores Barriga <it.victor.alfonso.flores@gmail.com>
2026-06-02 01:01:49 -0600

    feat: add application layer and user service

---
+++ backend/Fresam/Fresam.Application/Interfaces/IUsuarioService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Fresam.Application.DTOs.Usuarios;
+
+namespace Fresam.Application.Interfaces;
+
+public interface IUsuarioService
+{
+    List<UsuarioDto> ObtenerTodos();
+}
